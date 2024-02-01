d777c30754a5bff234956f5efdfe7ebca8debf69
IDGS80121000466 <21000466@alumnos.utleon.edu.mx>
2024-01-31 23:00:30 -0600

    EEG_31/01/2024_ Formulario alumno y Calcular distancia entre puntos

--- forms.py
+++ forms.py
@@ -13,3 +13,10 @@ class UserForm(Form):
     amaterno=StringField('amaterno')
     email=EmailField('email')
     edad=IntegerField('edad')
+
+class PuntosForm(FlaskForm):
+    x1 = IntegerField('x1')
+    x2 = IntegerField('x2')
+    y1 = IntegerField('y1')
+    y2 = IntegerField('y2')
+    resultado = IntegerField('resultado')
--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 from flask import Flask, render_template, request
 from flask import Flask, render_template, request, send_from_directory
-from forms import UserForm
+from forms import UserForm, PuntosForm
+
 import os
 import forms
+clave_secreta = os.urandom(24)
 
 app = Flask(__name__)
+app.secret_key = os.urandom(24)
 
 @app.route("/")
 def index():
@@ -18,10 +21,48 @@ def alumnos_route():
 
 @app.route("/alumnos2", methods=['GET', 'POST'])
 def alumnos():
+    nom=''
+    apa=''
+    ama=''
     alumno_clase=forms.UserForm(request.form)
     if request.method=='POST':
-        pass
-    return render_template("alumnos2.html",form=alumno_clase)
+        nom=alumno_clase.nombre.data
+        apa=alumno_clase.apaterno.data
+        ama=alumno_clase.amaterno.data
+        edad=alumno_clase.edad.data
+        print('Nombre: {}'.format(nom))
+        print('Apaterno: {}'.format(apa))
+        print('Amaterno: {}'.format(ama))
+    return render_template("alumnos2.html",form=alumno_clase,nom=nom,apa=apa,ama=ama)
+
+@app.route("/puntos", methods=['GET', 'POST'])
+def puntos():
+    x1 = x2 = y1 = y2 = ''
+    puntos_clase = PuntosForm(request.form)
+    if request.method == 'POST':
+        x1 = puntos_clase.x1.data
+        x2 = puntos_clase.x2.data
+        y1 = puntos_clase.y1.data
+        y2 = puntos_clase.y2.data
+        
+        resta1 = x2 - x1
+        resta2 = y2 - y1
+
+        distancia = (resta1 ** 2 + resta2 ** 2) ** 0.5
+
+        puntos_clase.resultado.data = distancia
+
+        print("La distancia entre los puntos es:", distancia)
+
+        print('x1: {}'.format(x1))
+        print('x2: {}'.format(x2))
+        print('y1: {}'.format(y1))
+        print('y2: {}'.format(y2))
+        print('resultado: {}'.format(distancia))
+
+    return render_template("distanciaPuntos.html", form=puntos_clase, x1=x1, x2=x2, y1=y1, y2=y2)
+
+
 
 @app.route("/maestros")
 def maestros():
@@ -105,3 +146,5 @@ def resul():
 
 if __name__ == "__main__":
     app.run(debug=True)
+
+app.secret_key = 'tu_clave_secreta_aqui'
